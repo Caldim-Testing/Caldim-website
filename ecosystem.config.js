c933bfa23f0ea86d7d9343e34eeb46346c6200d8
Caldim-Testing <monesha.raghuraman@caldimengg.in>
2026-08-11 17:57:43 +0530

    feat: add PM2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "caldim-website",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      instances: "max",
+      exec_mode: "cluster",
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
